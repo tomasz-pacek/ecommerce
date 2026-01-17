b96ab8aec56c250a55ca0f44da3e58921cb31ea9
nieukki <tomekpacek@o2.pl>
2026-01-17 08:57:59 +0100

    new here component for auth

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,22 @@
 import CardWrapper from "../_components/card-wrapper";
+import NewHere from "../_components/new-here";
 import PrismClient from "../_components/prism-client";
 import LoginForm from "./_components/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="relative w-screen h-screen flex items-center justify-center overflow-hidden">
+    <div className="relative flex h-screen w-screen items-center justify-center overflow-hidden">
       <div className="absolute inset-0 -z-10">
         <PrismClient />
       </div>
 
       <CardWrapper cardTitle="Login to your account">
         <LoginForm />
+        <NewHere
+          introText="New here?"
+          actionText="Create your account"
+          href="/register"
+        />
       </CardWrapper>
     </div>
   );
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,16 +1,22 @@
 import CardWrapper from "../_components/card-wrapper";
 import RegisterForm from "./_components/register-form";
 import PrismClient from "../_components/prism-client";
+import NewHere from "../_components/new-here";
 
 export default function RegisterPage() {
   return (
-    <div className="relative w-screen h-screen flex items-center justify-center overflow-hidden">
+    <div className="relative flex h-screen w-screen items-center justify-center overflow-hidden">
       <div className="absolute inset-0 -z-10">
         <PrismClient />
       </div>
 
       <CardWrapper cardTitle="Create your account">
         <RegisterForm />
+        <NewHere
+          introText="Already been here?"
+          actionText="Login to your account"
+          href="/login"
+        />
       </CardWrapper>
     </div>
   );
